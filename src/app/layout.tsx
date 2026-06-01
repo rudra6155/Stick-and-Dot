1ed35415529489d62ec801eefa8d2cea08001d00
rudra6155 <25rudra6155@gmail.com>
2026-06-01 19:26:57 +0530

    fix: remove h-full from html element to restore Lenis scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
     >
       <body className="cursor-none">
         <SmoothScroll>
